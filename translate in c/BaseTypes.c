cce45066b69baf48737a472137d62c661997e8d7
Mike Goppold von Lobsdorf <mgoppold5@gmail.com>
2019-10-04 13:14:38 -0400

    Imported some classes from similar GraphItNow project.

---
+++ translate in c/BaseTypes.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2019  Mike Goppold von Lobsdorf
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/*
+ * Basic types and constants
+ */
+
+#define bool short
+
+#define int8 char
+#define int16 short
+#define int32 int
+#define int64 long
+
+#define float32 float
+#define float64 double
+
+#define FALSE (0)
+#define TRUE (1)
---
+++ translate in c/TypicalStringUtils-design.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2019  Mike Goppold von Lobsdorf
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+int32 TypicalStringUtils_UnsafeStrGetLength(const int8 *str1) {
+	int32 i;
+
+	i = 0;
+	while(str1[i] != 0) {
+		i += 1;
+	}
+	
+	return i;
+}
+
+TypicalString * TypicalStringUtils_TypicalStringFromUnsafeStr(
+	const int8 *str1) {
+
+	int32 length;
+	int32 i;
+
+	length = TypicalStringUtils_UnsafeStrGetLength(str1);
+	
+	TypicalString *str2 = new TypicalString();
+	//str2->data = (int8 *) malloc(length);
+	str2->data = new int8[length];
+
+	i = 0;
+	while(i < length) {
+		str2->data[i] = str1[i];
+		i += 1;
+	}
+
+	str2->length = length;
+	str2->capacity = length;
+	return str2;
+}
+
